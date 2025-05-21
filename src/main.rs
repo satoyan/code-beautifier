99fc5770b8494221abed79cecdd7fe42cbc685d9
さとやｎ <satoyanxp@gmail.com>
2025-05-21 07:50:27 +0000

    feat: update dependencies and improve stdin handling in main function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Read, Write};
 use code_beautifier::{generate_html_from_source, parse_args};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let code = r#"fn main() { println!(\"Hello, world!\"); }"#;
+    let mut code = String::new();
+    // Read from stdin if there is input, otherwise use the default string
+    if atty::isnt(atty::Stream::Stdin) {
+        io::stdin().read_to_string(&mut code).expect("Failed to read from stdin");
+    } else {
+        code = r#"fn main() { println!(\"Hello, world!\"); }"#.to_string();
+    }
     let opts = parse_args(&args);
-    let html = generate_html_from_source(code, &opts.language);
+    let html = generate_html_from_source(&code, &opts.language);
 
     if opts.output_to_file {
         let mut file = File::create(&opts.output_filename)
